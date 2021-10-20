af44830602440b7f2a72f0ff6e6c4157c296c2cd
nexeranet <nuxkill@gmail.com>
2021-10-21 01:25:59 +0300

    add vis

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -2,147 +2,240 @@
 <template>
   <div class="container">
     <h3>Direct graph</h3>
-    <svg></svg>
+    <button @click="createPoint">Create Point</button>
+    <svg id="graph"></svg>
   </div>
 </template>
 
 <script>
-import data from "./data.js";
+import { generateData, createNode } from "./data.js";
 import * as d3 from "d3";
 
+let data = generateData(10);
 export default {
   data() {
     return {
-      count: 0,
+      data: data,
+      width: 0,
+      height: 0,
     };
   },
-  mounted() {
-    var svg = d3.select("svg");
-    let container = document.querySelector(".container");
-    let svg_node = document.querySelector("svg");
-    svg_node.setAttribute("width", svg_node.clientWidth);
-    svg.attr("height", window.innerHeight - 30);
-    svg.attr(
-      "height",
-      svg_node.clientWidth / 2 > 400 ? svg_node.clientWidth / 2 : 400
-    );
-    var width = +svg.attr("width"),
-      height = +svg.attr("height");
+  methods: {
+    initSVG() {
+      var svg = d3.select("svg");
+      let svg_node = document.querySelector("svg");
+      svg_node.setAttribute("width", svg_node.clientWidth);
+      svg.attr("height", window.innerHeight - 30);
+      svg.attr(
+        "height",
+        svg_node.clientWidth / 2 > 400 ? svg_node.clientWidth / 2 : 400
+      );
+      const marker = svg.append("defs").append("marker");
+      const attrs = {
+        id: "arrowhead",
+        viewBox: "-0 -5 10 10",
+        refX: 5,
+        refY: 0,
+        orient: "auto",
+        markerWidth: 5,
+        markerHeight: 5,
+        xoverflow: "visible",
+      };
+      for (const [key, value] of Object.entries(attrs)) {
+        marker.attr(key, value);
+      }
+      marker
+        .append("svg:path")
+        .attr("d", "M 0,-5 L 10 ,0 L 0,5")
+        .attr("fill", "lightblue")
+        .style("stroke", "none");
 
-    var color = d3.scaleOrdinal(d3.schemeAccent);
+      svg.append("g").attr("id", "group");
+      const width = +svg.attr("width"),
+        height = +svg.attr("height");
+      this.width = width;
+      this.height = height;
+    },
+    createPoint() {
+      let [node, links] = createNode(data.nodes.length);
+      data = {
+        nodes: [...data.nodes, node],
+        links: [...data.links, ...links],
+      };
+      d3.select("#group").html("");
+      this.render(data);
+    },
+    render(data) {
+      const width = this.width,
+        height = this.height;
+      let colors = d3.schemeTableau10;
+      let nodeGroups = undefined;
+      const invalidation = undefined;
+      const linkStrokeLinecap = "round";
+      const nodeFill = "currentColor",
+        nodeStroke = "#fff", // node stroke color
+        nodeStrokeWidth = 1.5, // node stroke width, in pixels
+        nodeStrokeOpacity = 1;
+      const linkStrokeWidth = (l) => Math.sqrt(l.value);
+      const linkStrokeOpacity = 1;
+      const nodeGroup = (d) => d.group;
+      const nodeTitle = (d) => `${d.id}`;
+      const nodeId = (d) => d.id;
+      const linkSource = ({ source }) => source;
+      const linkTarget = ({ target }) => target;
+      const N = d3.map(data.nodes, nodeId).map(intern);
+      const LS = d3.map(data.links, linkSource).map(intern);
+      const LT = d3.map(data.links, linkTarget).map(intern);
+      if (nodeTitle === undefined) nodeTitle = (_, i) => N[i];
+      const T = nodeTitle == null ? null : d3.map(data.nodes, nodeTitle);
+      const G =
+        nodeGroup == null ? null : d3.map(data.nodes, nodeGroup).map(intern);
+      const W =
+        typeof linkStrokeWidth !== "function"
+          ? null
+          : d3.map(data.links, linkStrokeWidth);
 
-    let graph = data;
+      // Replace the input nodes and links with mutable objects for the simulation.
+      let nodes = d3.map(data.nodes, (val, i) => ({
+        id: N[i],
+        name: val.name,
+        weight: val.weight,
+      }));
+      let links = d3.map(data.links, (val, i) => ({
+        source: LS[i],
+        target: LT[i],
+        stroke: val.stroke,
+      }));
 
-    var simulation = d3
-      .forceSimulation()
-      .force(
-        "link",
-        d3.forceLink().id(function (d) {
-          return d.id;
+      // Compute default domains.
+      if (G && nodeGroups === undefined) nodeGroups = d3.sort(G);
+
+      // Construct the scales.
+      const color =
+        nodeGroup == null ? null : d3.scaleOrdinal(nodeGroups, colors);
+
+      const simulation = d3
+        .forceSimulation(nodes)
+        .alphaDecay(0.03)
+        .force(
+          "link",
+          d3
+            .forceLink(links)
+            .id(({ index: i }) => N[i])
+            .distance(200)
+        )
+        .force("charge", d3.forceManyBody().strength(-300))
+        .force("attraceForce", d3.forceManyBody().strength(-100))
+        .force("center", d3.forceCenter());
+      simulation.on("tick", ticked);
+
+      const svg = d3
+        .select("svg")
+        .attr("viewBox", [-width / 2, -height / 2, width, height])
+        .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+      const g = svg.select("#group");
+      const link = g
+        .append("g")
+        .attr("stroke-linecap", linkStrokeLinecap)
+        .attr("class", "links")
+        .selectAll("line")
+        .data(links)
+        .join("line")
+        .attr("stroke-width", function (d) {
+          return Math.sqrt(d.value);
         })
-      )
-      .force(
-        "charge",
-        d3
-          .forceManyBody()
-          .strength(-500)
-          .distanceMax(width / 3)
-          .distanceMin(10)
-      )
-      .force("center", d3.forceCenter(width / 2, height / 2));
-
-    var link = svg
-      .append("g")
-      .attr("class", "links")
-      .selectAll("line")
-      .data(graph.links)
-      .enter()
-      .append("line")
-      .attr("stroke", function (d) {
-        return d.stroke;
-      })
-      .attr("stroke-width", function (d) {
-        return Math.sqrt(d.value);
-      });
-
-    var node = svg
-      .append("g")
-      .attr("class", "nodes")
-      .selectAll("g")
-      .data(graph.nodes)
-      .enter()
-      .append("g")
-      .attr("x", function (d) {
-        return d.x;
-      })
-      .attr("y", function (d) {
-        return d.y;
-      });
-    var circles = node
-      .append("circle")
-      .attr("r", function (d) {
-        return d.weight;
-      })
-      .attr("fill", function (d) {
-        return color(d.name);
-      });
-
-    var lables = node
-      .append("text")
-      .text(function (d) {
-        return d.name;
-      })
-      .attr("x", 6)
-      .attr("y", 3);
-
-    node.append("title").text(function (d) {
-      return d.name;
-    });
-
-    simulation.nodes(graph.nodes).on("tick", ticked);
-
-    simulation.force("link").links(graph.links);
-
-    function ticked() {
-      link
-        .attr("x1", function (d) {
-          return d.source.x;
-        })
-        .attr("y1", function (d) {
-          return d.source.y;
-        })
-        .attr("x2", function (d) {
-          return d.target.x;
-        })
-        .attr("y2", function (d) {
-          return d.target.y;
+        .attr("marker-end", "url(#arrowhead)")
+        .attr("stroke", (d) => {
+          return d.stroke;
         });
 
-      node.attr("transform", function (d) {
-        return "translate(" + d.x + "," + d.y + ")";
-      });
-    }
+      const circles_group = g
+        .append("g")
+        .attr("stroke-opacity", nodeStrokeOpacity)
+        .attr("stroke-width", nodeStrokeWidth)
+        .selectAll("circle")
+        .data(nodes)
+        .join("g");
 
-    function dragstarted(d) {
-      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-      d.fx = d.x;
-      d.fy = d.y;
-    }
+      circles_group.append("circle").attr("r", (d) => d.weight);
+      circles_group
+        .append("text")
+        .text(function (d) {
+          return `${d.name}`;
+        })
+        .attr("stroke", "white")
+        .style("font-size", "16px")
+        .attr("x", (d) => {
+          return -(d.weight / 2);
+        })
+        .attr("y", (d) => d.weight / 2);
+      const node = circles_group.call(drag(simulation));
 
-    function dragged(d) {
-      dfx = d3.event.x;
-      d.fy = d3.event.y;
-    }
+      if (W) link.attr("stroke-width", ({ index: i }) => W[i]);
+      if (G) node.attr("fill", ({ index: i }) => color(G[i]));
+      if (T) node.append("title").text(({ index: i }) => T[i]);
+      if (invalidation != null) invalidation.then(() => simulation.stop());
 
-    function dragended(d) {
-      if (!d3.event.active) simulation.alphaTarget(0);
-      d.fx = null;
-      d.fy = null;
-    }
+      function intern(value) {
+        return value !== null && typeof value === "object"
+          ? value.valueOf()
+          : value;
+      }
+
+      function drag(simulation) {
+        function dragstarted(event) {
+          if (!event.active) simulation.alphaTarget(0.3).restart();
+          event.subject.fx = event.subject.x;
+          event.subject.fy = event.subject.y;
+        }
+
+        function dragged(event) {
+          event.subject.fx = event.x;
+          event.subject.fy = event.y;
+        }
+
+        function dragended(event) {
+          if (!event.active) simulation.alphaTarget(0);
+          event.subject.fx = null;
+          event.subject.fy = null;
+        }
+
+        return d3
+          .drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended);
+      }
+
+      function ticked() {
+        link
+          .attr("x1", (d) => d.source.x)
+          .attr("y1", (d) => d.source.y)
+          .attr("x2", (d) => d.target.x)
+          .attr("y2", (d) => d.target.y);
+
+        node.attr("transform", function (d) {
+          return "translate(" + d.x + "," + d.y + ")";
+        });
+      }
+      svg.call(
+        d3.zoom().on("zoom", function (event) {
+          g.attr("transform", event.transform);
+        })
+      );
+    },
+  },
+  mounted() {
+    this.initSVG();
+    this.render(data);
+    console.log("START");
   },
 };
 </script>
 <style scoped>
+text {
+  font-size: 10px;
+}
 .container {
   padding: 15px;
   box-sizing: border-box;
--- src/components/data.js
+++ src/components/data.js
@@ -1,30 +1,50 @@
 function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
-function generateData(num){
-  let nodes = [], links = [];
-  for(let i = 0; i < num; i++) {
+
+export function createNode(length) {
+  let Node = {
+    id: length,
+    name: length,
+    weight: randomIntFromInterval(10, 30)
+  };
+  let links = [];
+  let num_of_links = randomIntFromInterval(1, 4);
+  for (let i = 0; i < num_of_links; i++) {
+    links.push({
+      source: Node.id,
+      target: randomIntFromInterval(0, length) == Node.id ? 0 : randomIntFromInterval(0, length - 1),
+      value: randomIntFromInterval(0, 50),
+      stroke: "lightblue",
+    })
+  }
+  return [Node, links]
+}
+
+export function generateData(num) {
+  let nodes = [],
+    links = [];
+  for (let i = 0; i < num; i++) {
     nodes.push({
       id: i,
       name: i,
-      weight: randomIntFromInterval(10, num + 10)
+      weight: randomIntFromInterval(10, 30)
     })
   }
   for (let node of nodes) {
-      let num_of_links = randomIntFromInterval(1, 4)
-      for(let i = 0; i < num_of_links; i ++){
-        links.push({
-          source: node.id,
-          target: randomIntFromInterval(0, nodes.length - 1),
-          value: randomIntFromInterval(0, num),
-          stroke: "lightblue",
-        })
-      }
+    let num_of_links = randomIntFromInterval(1, 4)
+    for (let i = 0; i < num_of_links; i++) {
+      links.push({
+        source: node.id,
+        target: randomIntFromInterval(0, nodes.length - 1) == node.id ? 0 : randomIntFromInterval(0, nodes.length - 1),
+        value: randomIntFromInterval(0, 50),
+        stroke: "lightblue",
+      })
+    }
   }
+
   return {
     nodes: nodes,
     links: links,
   }
 }
-const data =  generateData(50);
-export default data; 
